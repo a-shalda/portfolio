c1ab0392a513e164cb6bdaeeabdab375955d30a3
Alex Shalda <123298731+a-shalda@users.noreply.github.com>
2023-09-07 21:03:17 +0400

    adding mockup image

--- darkmode.js
+++ darkmode.js
@@ -1,11 +1,25 @@
 const colorSchemeToggle = document.querySelector('.header__left__button');
+let projectMockup = document.querySelector('.project_one__image');
 
 colorSchemeToggle.addEventListener('pointerdown', () => {
   document.body.classList.toggle('darkmode');
+  if (projectMockup === 'images/white.jpg') {
+    console.log('hey');
+    // document.querySelector('.project_one__image').src = 'images/black.jpg';
+  }
+  // else {
+  //   console.log('hey');
+
+  //   document.querySelector('.project_one__image').src = 'images/black.jpg';
+  // }
 })
 
 const useDark = window.matchMedia("(prefers-color-scheme: dark)");
 
-if (useDark) {
-  document.body.classList.toggle('darkmode');
+
+if (projectMockup.src === 'images/white.jpg') {
+  console.log('hey');
+  // document.querySelector('.project_one__image').src = 'images/black.jpg';
 }
+
+console.log(projectMockup.src);
