42682f63c6640d55f3fb99104fd86b5d1faa4a92
Alex Shalda <123298731+a-shalda@users.noreply.github.com>
2023-09-08 13:04:45 +0400

    set breakpoints for the image

--- darkmode.js
+++ darkmode.js
@@ -1,11 +1,14 @@
 const colorSchemeToggle = document.querySelector('.header__left__button');
+let projectMockup = document.querySelector('.main__project');
 const imageWhite = document.querySelector('.main__project__one__image--white');
 const imageBlack = document.querySelector('.main__project__one__image--black');
+
 
 
 colorSchemeToggle.addEventListener('pointerdown', () => {
   document.body.classList.toggle('darkmode');
   imageWhite.classList.toggle('main__project__one__image--hidden');
+
 })
 
 const useDark = window.matchMedia("(prefers-color-scheme: dark)");
