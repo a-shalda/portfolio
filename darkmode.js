e6ece9c2d9c4bd70bc2d1aae62b590d4b4648cbd
Alex Shalda <123298731+a-shalda@users.noreply.github.com>
2023-09-08 14:29:16 +0400

    added pulsing animation

--- darkmode.js
+++ darkmode.js
@@ -13,7 +13,30 @@ colorSchemeToggle.addEventListener('pointerdown', () => {
 
 const useDark = window.matchMedia("(prefers-color-scheme: dark)");
 
-if (useDark) {
-  document.body.classList.toggle('darkmode');
-  imageWhite.classList.toggle('main__project__one__image--hidden');
-}
+// if (useDark) {
+//   document.body.classList.toggle('darkmode');
+//   imageWhite.classList.toggle('main__project__one__image--hidden');
+// }
+
+const imageCont = document.querySelector('.main__project__one__image');
+
+imageCont.classList.add('cont_blurred');
+
+const imageContFirst = imageCont.querySelector('.main__project__one__image--black');
+const imageContSecond = imageCont.querySelector('.main__project__one__image--white');
+
+imageContFirst.classList.add('img_unloaded');
+imageContSecond.classList.add('img_unloaded');
+
+imageContFirst.addEventListener('load', () => {
+    imageContFirst.classList.remove('img_unloaded');
+    imageCont.classList.remove('cont_blurred');
+    console.log('first');
+})
+
+imageContSecond.addEventListener('load', () => {
+  imageContSecond.classList.remove('img_unloaded');
+  imageCont.classList.remove('cont_blurred');
+  console.log('second');
+})
+
