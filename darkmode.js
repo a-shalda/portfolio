48be86829d69045bfac06d568ab9477c90307313
Alex Shalda <123298731+a-shalda@users.noreply.github.com>
2023-09-08 11:08:29 +0400

    images change without flickering

--- darkmode.js
+++ darkmode.js
@@ -1,22 +1,16 @@
 const colorSchemeToggle = document.querySelector('.header__left__button');
-let projectMockup = document.querySelector('.project_one__image');
+const imageWhite = document.querySelector('.main__project__one__image--white');
+const imageBlack = document.querySelector('.main__project__one__image--black');
+
 
 colorSchemeToggle.addEventListener('pointerdown', () => {
   document.body.classList.toggle('darkmode');
-
-  if (projectMockup.src === 'https://a-shalda.github.io/portfolio/images/white.jpg') {
-    document.querySelector('.project_one__image').src = 'https://a-shalda.github.io/portfolio/images/black.jpg';
-  }
-  else {
-    document.querySelector('.project_one__image').src = 'https://a-shalda.github.io/portfolio/images/white.jpg';
-  }
+  imageWhite.classList.toggle('main__project__one__image--hidden');
 })
 
 const useDark = window.matchMedia("(prefers-color-scheme: dark)");
 
-
-
 if (useDark) {
   document.body.classList.toggle('darkmode');
-  document.querySelector('.project_one__image').src = 'https://a-shalda.github.io/portfolio/images/black.jpg';
+  imageWhite.classList.toggle('main__project__one__image--hidden');
 }
